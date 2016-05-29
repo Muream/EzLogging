2296f47e81c3fa090989d4ecb066e4971eba69d1
Muream <muream@gmail.com>
2016-05-29 23:49:13 +0200

    Cleanup

--- AutoDerush.py
+++ AutoDerush.py
@@ -1,29 +1,30 @@
+import subprocess as sp
+import os
+import glob
+
 '''
 TODO :
-- Make sure the timers are correctly merged
+- Make sure the timings are correctly merged
 - Make sure the range of the clip is the right one
-- Do something to properly tell the user how many timers there are for the current video and which timers is currently processed
-- Use the same settings as EzLogging, maybe create another file that handles the config and call it in both scripts?
-- Make a variable containing the video format instead of blindly guessing it's mp4
+- Do something to properly tell the user how many timings there are for the
+current video and which timings is currently processed
+- Use the same settings as EzLogging, maybe create another file that handles
+    the config and call it in both scripts?
+- Make a variable containing the video format instead of blindly guessing it's
+    mp4
 '''
 
 '''
 Dependencies
 FFMPEG : https://ffmpeg.org/download.html
-
 '''
+# Temporary until I have handled settings change this depending on your configuration.
 
-import subprocess as sp
-import os
-import glob
+# Windows
+# FFMPEG_BIN = "C:/Program Files/ffmpeg-20160522-git-566be4f-win64-static/bin/ffmpeg.exe"
+# sourceFolder = "D:/Videos/__Youtube/__Rushes_to_sort/test/"
 
-#Temporary until I have handled settings change this depending on your configuration.
-
-#Windows
-#FFMPEG_BIN = "C:/Program Files/ffmpeg-20160522-git-566be4f-win64-static/bin/ffmpeg.exe"
-#sourceFolder = "D:/Videos/__Youtube/__Rushes_to_sort/test/"
-
-#linux
+# Linux
 FFMPEG_BIN = "/usr/bin/ffmpeg"
 sourceFolder = "/home/muream/Videos/test/"
 
@@ -32,27 +33,29 @@ fps = 30
 before = 20
 after = 10
 
-def ConvertTime(timer):
+
+def convert_time(timing):
     '''
-    Converts the timer in seconds
-    :param timer: the timer with a format hh:mm:ss
+    Converts the timing in seconds
+    :param timing: the timing with a format hh:mm:ss
     :return: time in seconds
     '''
-    hour = int(timer[0:2])
-    minute = int(timer[3:5])
-    second = int(timer[6:8])
+    hour = int(timing[0:2])
+    minute = int(timing[3:5])
+    second = int(timing[6:8])
     hour = hour*60*60
     minute = minute*60
     second = second
     seconds = hour+minute+second
     return seconds
 
-def GetRange(seconds, before, after):
+
+def get_range(seconds, before, after):
     '''
     Gets the range of the clip
-    :param seconds: the timer in seconds
-    :param before: number of seconds before the timer
-    :param after: number of seconds after the timer
+    :param seconds: the timing in seconds
+    :param before: number of seconds before the timing
+    :param after: number of seconds after the timing
     :return: the range in a list
     '''
     start = seconds - before
@@ -60,10 +63,11 @@ def GetRange(seconds, before, after):
         start = 0
     end = seconds + after
     length = end - start
-    clipRange = [start, end, length]
+    clipRange = {'Start': start,'End': end,'Length': length}
     return clipRange
 
-def GetTimings(textFile):
+
+def get_timings(textFile):
     '''
     Puts the content of a textfile in a list
     :param textFile: path to the text file
@@ -72,32 +76,32 @@ def GetTimings(textFile):
     '''
     f = open(textFile, 'r')
     lines = f.readlines()
-    timers = []
+    timings = []
     for line in lines:
         line = line[:-1]
-        seconds = ConvertTime(line)
-        timers.append(seconds)
-        #print timers
-    return timers
+        seconds = convert_time(line)
+        timings.append(seconds)
+    return timings
 
-def ExportClip(filename, start, length, targetname):
+
+def export_clip(filename, start, length, targetname):
     '''
     :param filename: path to the original file
     :param start: start of the clip
     :param end: end of the clip
     :param targetname: path to the exported file
     '''
-    command = ["ffmpeg",
-            "-i", filename,
-            "-map", "0:0",
-            "-map", "0:1",
-            "-map", "0:2",
-            "-map", "0:3",
-            "-map", "0:4",
-            "-ss", "%0.2f" % start,
-            "-t", "%0.2f" % length,
-            "-codec", "copy",
-            targetname]
+    command  = ["ffmpeg",
+                "-i", filename,
+                "-map", "0:0",
+                "-map", "0:1",
+                "-map", "0:2",
+                "-map", "0:3",
+                "-map", "0:4",
+                "-ss", "%0.2f" % start,
+                "-t", "%0.2f" % length,
+                "-codec", "copy",
+                targetname]
 
     p = sp.Popen(command,
                  stdin=sp.PIPE,
@@ -105,58 +109,95 @@ def ExportClip(filename, start, length, targetname):
     output = p.communicate('S\nL\n')[0]
     print output
 
+'''
+def can_merge(timings, currentIndex, after):
+    \'''
+    checks if the timing can be merged (not if they should)
+    need to know if:
+    - we are at the last timing
+    -
+    \'''
+    # Do some work here
+    if currentIndex + 1 == len(timings):
+        lastTiming = True
+    else:
+        lastTiming = False
+'''
+
+def clip_informations(timings, currentIndex, after):
+    # Compares current timing to the next one, if they are to close : Merges them and extends the range
+    currentTiming = timings[currentIndex]
+
+    if currentIndex + 1 == len(timings):
+        lastTiming = True
+        difference = 0
+        extended = False
+    else:
+        lastTiming = False
+    # Merge timings until two are to far appart or if there's nothing to merge
+    while lastTiming is False:
+        currentTiming = timings[currentIndex]
+        nextTiming = timings[currentIndex + 1]
+        difference = nextTiming - currentTiming
+        if difference <= after:
+            # merge the timings here
+            afterTemp = after + difference  # used to add some time to the range if timings are too close to each other
+            del nextTiming  # remove the merged timing so that it's not processed twice
+            clipRange = get_range(currentTiming, before, afterTemp)
+            extended = True
+        else:
+            # break the loop here
+            extended = False
+            break
+
+        currentIndex += 1
+    clipRange = get_range(currentTiming, before, after)
+    start = clipRange['Start']
+    end = clipRange['End']
+    length = clipRange['Length']
+    clipInfo = {'Difference': difference, 'Extended': extended, 'Start': start, 'End': end, 'Length': length, 'Last Timer': lastTiming}
+    return clipInfo
 
 
 def Main():
-    #Creates the "Derush" Folder
-    exportPath = sourceFolder+"Derush"
+    # Creates the "Derush" Folder
+    exportPath = sourceFolder + "Derush"
     if not os.path.exists(exportPath):
         os.makedirs(exportPath)
 
-    #loops through all the video files in said folder
+    # Loops through all the video files in said folder
     videoFiles = glob.glob(sourceFolder+"*.mp4")
     for videoFile in videoFiles:
         currentFile = videoFiles.index(videoFile) + 1
         numberOfFiles = len(videoFiles)
-        name = os.path.basename(videoFile.rsplit('.',1)[0]) #Gets the name of the file
+        name = os.path.basename(videoFile.rsplit('.', 1)[0])  # Gets the name of the file
         textFile = sourceFolder+name+".txt"
-        timers = GetTimings(textFile)
-        #Loops through all the timers for the Video file
-        for timing in timers:
-            afterTemp = after
-            currentTimer = timers.index(timing)
-            numberOfTimers = len(timers)
+        timings = get_timings(textFile)
+        #Loops through all the timings for the Video file
+        for timing in timings:
+            currentIndex = timings.index(timing)
+            numberOfTimings = len(timings)
             print "Processing file " + str(currentFile) + "/" + str(numberOfFiles)
-            print "Processing Timer " + str(currentTimer+1) + "/" + str(numberOfTimers)
-           
-            
-            print "Timer: " + str(timing) +"s"
-            #Compares current timer to the next one, if they are to close : Merges them and extends the range
-            while True and currentTimer < len(timers)-1:
-                difference = timers[currentTimer+1] - timers[currentTimer]
-                print "Time until next timer = %is" % difference
-                if difference <= after:
-                    #merge the timers here
-                    afterTemp = after + difference # used to add some time to the range if timers are too close to each other
-                    del timers[currentTimer+1] #remove the merged timer so that it's not processed twice
-                    print "Extended by %is" % difference
+            print "Processing Timing " + str(currentIndex+1) + "/" + str(numberOfTimings)
+            print "Timing: " + str(timing) +"s"
+            # Compares current timing to the next one, if they are to close : Merges them and extends the range
+            clipInfo = clip_informations(timings, currentIndex, after)
+
+            print "Seconds until next Timing : %is" % clipInfo['Difference']
+
+            if clipInfo['Last Timer'] == False:
+                if clipInfo['Extended'] == True:
+                    print "The clip has been extended by %is" % clipInfo['Difference']
                 else:
-                    #break the loop here
-                    print "Not extended"
-                    break #should be stopping the loop (maybe it is working and the for loop makes the while restart where it was)
-                print "\n"
-                currentTimer += 1
-            if afterTemp > after:
-                clipRange = GetRange(timing, before, afterTemp)
+                    print "The clip has not been extended"
             else:
-                clipRange = GetRange(timing, before, after)
-            start = clipRange[0]
-            end = clipRange[1]
-            length = clipRange[2]
-            print "Start: " + str(start) +"s"
-            print "End: " + str(end) +"s"
-            print "Length: " + str(length) +"s"
+                print "This is the last timing"
+
+            print "Clip Start: " + str(clipInfo['Start']) + "s"
+            print "Clip End: " + str(clipInfo['End']) + "s"
+            print "Clip Duration: " + str(clipInfo['Length']) + "s"
+
             print "\n"
-            clip = ExportClip(videoFile, start, length, exportPath+"/%s_Clip_%s.mp4" % (name, str(currentTimer+1).zfill(2)))
+            # clip = export_clip(videoFile, clipInfo['Start'], clipInfo['Length'], exportPath+"/%s_Clip_%s.mp4" % (name, str(currentTiming+1).zfill(2)))
 
 Main()
--- Ezlogging.py
+++ Ezlogging.py
@@ -167,4 +167,3 @@ def main():
 
 
 main()
-
