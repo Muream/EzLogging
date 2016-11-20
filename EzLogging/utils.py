75d66ae2f73bb0c5d63aa9bd3589b6ce82a2667b
Muream <muream@gmail.com>
2016-11-20 01:11:54 +0100

    add convert_string()

--- EzLogging/utils.py
+++ EzLogging/utils.py
@@ -3,3 +3,18 @@ def check_path(path):
     if path[-1:] == '/':
         path = path[:-1]
     return path
+
+
+def convert_string(string):
+    newString = ""
+    for index, char in enumerate(string):
+        try:
+            nextChar = string[index + 1]
+        except IndexError:
+            nextChar = char
+        if char.isalpha() or char.isdigit():
+            newString = newString + char
+        elif nextChar.isalpha() or nextChar.isdigit():
+            newString = newString + "_"
+        else:
+            continue
