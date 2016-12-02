374632a7504a2920b26b1160a7576a9dec23c1bd
Muream <muream@gmail.com>
2016-12-02 09:32:07 +0100

    gameDetector Ready

--- EzLogging/EzLoggingUI.py
+++ EzLogging/EzLoggingUI.py
@@ -4,12 +4,14 @@ import os
 
 from pyhooked import Hook, KeyboardEvent
 import PySide.QtGui as QtGui
+import PySide.QtCore as QtCore
 
 import Ezlogging
 import AutoLog
 import gameDetector
 from Config import Settings
 import settingsDialog
+import addGameDialog
 
 
 class EzLoggingUI(QtGui.QMainWindow):
@@ -34,7 +36,8 @@ class EzLoggingUI(QtGui.QMainWindow):
 
         self.create_menus()
         self.create_central_widget()
-        self.create_info_bar()
+        # self.create_info_bar()
+        self.create_games_bar()
         self.create_log_output()
         self.hotkeys()
 
@@ -75,31 +78,49 @@ class EzLoggingUI(QtGui.QMainWindow):
 
     def create_info_bar(self):
         self.infoBarLayout = QtGui.QHBoxLayout()
+        self.infoBarLayout.setAlignment(QtCore.Qt.AlignLeft)
 
+        self.startRecordLabel = QtGui.QLabel("Start recording : {}".format(str(self.settings.startRecord)))
+        self.startRecordLabel.setFont(self.labelsFont)
+        self.infoBarLayout.addWidget(self.startRecordLabel)
+
+        self.logTimeLabel = QtGui.QLabel("Log Time : {}".format(str(self.settings.logTime)))
+        self.logTimeLabel.setFont(self.labelsFont)
+        self.infoBarLayout.addWidget(self.logTimeLabel)
+
+        self.stopRecordLabel = QtGui.QLabel("Stop recording : {}".format(str(self.settings.stopRecord)))
+        self.stopRecordLabel.setFont(self.labelsFont)
+        self.infoBarLayout.addWidget(self.stopRecordLabel)
+
+    def create_games_bar(self):
+        # Games Bar Layout
+        self.gamesBarLayout = QtGui.QHBoxLayout()
+
+        # Running Games Layout
+        self.gamesBarLayout = QtGui.QHBoxLayout()
+        self.gamesBarLayout.setAlignment(QtCore.Qt.AlignLeft)
+
+        # Font Stuff
         self.labelsFont = QtGui.QFont()
         self.labelsFont.setPointSize(10)
-        #
-        # self.startRecordLabel = QtGui.QLabel("Start recording : {}".format(str(self.settings.startRecord)))
-        # self.startRecordLabel.setFont(self.labelsFont)
-        # self.infoBarLayout.addWidget(self.startRecordLabel)
-        #
-        # self.logTimeLabel = QtGui.QLabel("Log Time : {}".format(str(self.settings.logTime)))
-        # self.logTimeLabel.setFont(self.labelsFont)
-        # self.infoBarLayout.addWidget(self.logTimeLabel)
-        #
-        # self.stopRecordLabel = QtGui.QLabel("Stop recording : {}".format(str(self.settings.stopRecord)))
-        # self.stopRecordLabel.setFont(self.labelsFont)
-        # self.infoBarLayout.addWidget(self.stopRecordLabel)
-        self.runningGamesLabel = QtGui.QLabel("Running Games")
-        self.runningGamesLabel.setFont(self.labelsFont)
-        self.infoBarLayout.addWidget(self.runningGamesLabel)
 
+        # Running games Combo Box
         self.runningGamesComboBox = QtGui.QComboBox()
-        self.infoBarLayout.addWidget(self.runningGamesComboBox)
+        self.gamesBarLayout.addWidget(self.runningGamesComboBox)
+
+        self.refreshGamesButton = QtGui.QPushButton("Refresh")
+        self.refreshGamesButton.released.connect(self.update_running_games)
+        self.gamesBarLayout.addWidget(self.refreshGamesButton)
+        self
+
+        # Add Game Button
+        self.addGameButton = QtGui.QPushButton("Add Game")
+        self.addGameButton.released.connect(self.add_game)
+        self.gamesBarLayout.addWidget(self.addGameButton)
 
         self.update_running_games()
 
-        self.centralWidget().layout().addLayout(self.infoBarLayout)
+        self.centralWidget().layout().addLayout(self.gamesBarLayout)
 
     def create_log_output(self):
         """The log output, where all the info are printed."""
@@ -123,41 +144,42 @@ class EzLoggingUI(QtGui.QMainWindow):
         self.settingsDialog.show()
 
     def start_autolog(self):
-        thread = threading.Thread(target=AutoLog.main, args=(self.settings, self))
-        thread.start()
+        autoLogThread = threading.Thread(target=AutoLog.main, args=(self.settings, self))
+        autoLogThread.daemon = True
+        autoLogThread.start()
 
     def handle_events(self, args):
         """Global hotkeys actions"""
         if isinstance(args, KeyboardEvent):
             if args.current_key == self.settings.startRecord and args.event_type == 'key down':
                 self.f.createfile(self)
-                self.update_running_games()
             if args.current_key == self.settings.logTime and args.event_type == 'key down':
                 self.f.writetime(self)
-                self.update_running_games()
             if args.current_key == self.settings.stopRecord and args.event_type == 'key down':
                 self.f.closefile(self)
-                self.update_running_games()
+        return
 
     def hotkeys(self):
         """Global hotkeys setup."""
         self.f = Ezlogging.TextFile(self.settings)
         hk = Hook()
         hk.handler = self.handle_events
-        thread = threading.Thread(target=hk.hook)
-        thread.start()
+        hotkeyThread = threading.Thread(target=hk.hook)
+        hotkeyThread.daemon = True
+        hotkeyThread.start()
 
     def update_ui(self):
         self.settings.read_config()
-        self.startRecordLabel.setText("Start recording : {}".format(str(self.settings.startRecord)))
+        # self.startRecordLabel.setText("Start recording : {}".format(str(self.settings.startRecord)))
 
-        self.logTimeLabel.setText("Log Time : {}".format(str(self.settings.logTime)))
+        # self.logTimeLabel.setText("Log Time : {}".format(str(self.settings.logTime)))
 
-        self.stopRecordLabel.setText("Stop recording : {}".format(str(self.settings.stopRecord)))
+        # self.stopRecordLabel.setText("Stop recording : {}".format(str(self.settings.stopRecord)))
 
     def update_running_games(self):
         games = gameDetector.running_games()
         print games
+
         self.runningGamesComboBox.clear()
         for game in games:
             self.runningGamesComboBox.addItem(game)
@@ -165,7 +187,10 @@ class EzLoggingUI(QtGui.QMainWindow):
             index = self.runningGamesComboBox.findText(self.currentGame)
             self.runningGamesComboBox.setCurrentIndex(index)
         self.currentGame = self.runningGamesComboBox.currentText()
-        print self.currentGame
+
+    def add_game(self):
+        self.addGameDialog = addGameDialog.AddGameDialog(self)
+        self.addGameDialog.show()
 
 
 def show():
