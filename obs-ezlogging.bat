c913e9d1d750886d8c8b4db21abd25117c53b351
Muream <muream@gmail.com>
2018-10-23 15:37:09 +0200

    initial commit

---
+++ obs-ezlogging.bat
@@ -0,0 +1,5 @@
+@echo off
+start C:\Users\muream\Envs\ezlogging\Scripts\pythonw.exe C:\Users\muream\projects\EzLogging\main.py
+cd "C:\Program Files (x86)\obs-studio\bin\64bit"
+start obs64.exe
+exit
